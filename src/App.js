1f5f8233593b1e533ed24227ae2039677cd5b85e
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-24 09:21:04 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <EventDemo />
+    </div>
+  );
+}
+
+function EventDemo() {
+  // Data Member
+  let [counter] = useState(100);
+
+  // Member Funcn
+  let likeMe = () => {
+    alert();
+  };
+
+  // UI
+  return (
+    <div>
+      <h1>Like {counter}</h1>
+      <input type="button" value="Like Me" onClick={likeMe} />
     </div>
   );
 }
