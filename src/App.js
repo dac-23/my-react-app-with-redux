a7ae97735eb0ed385c8f4af80f9970e15deba48b
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-24 12:45:32 +0530

    basic todo.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,69 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { deposit, withdraw } from "./redux/accountSlice";
 
 function App() {
   return (
     <div>
-      <DepositAccount />
+      <Todo />
+    </div>
+  );
+}
+
+function Todo() {
+  let [list, setList] = useState([]);
+  let [todo, setTodo] = useState("");
+
+  let inputTodoHandler = (e) => {
+    setTodo(e.target.value);
+  };
+
+  let addTodoToDb = async () => {
+    let url = "http://localhost:4000/addtodo";
+
+    let data = {
+      message: todo,
+    };
+
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodo("");
+    // alert("Todo Added");
+    getTodos();
+  };
+
+  let getTodos = async () => {
+    let url = "http://localhost:4000/";
+    let res = await fetch(url);
+    let data = await res.json();
+
+    console.log(data);
+    setList(data);
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+
+      <input
+        type="text"
+        placeholder="Addo Todo"
+        value={todo}
+        onChange={inputTodoHandler}
+      />
+      <input type="button" value="Add Todo" onClick={addTodoToDb} />
+      <input type="button" value="GET TODOS" onClick={getTodos} />
       <hr />
-      <WithdrawlAccount />
-    </div>
-  );
-}
 
-function DepositAccount() {
-  // State is global here
-  let { account } = useSelector((state) => state);
-  let dispatch = useDispatch();
-
-  let depositHandler = () => {
-    dispatch(deposit());
-  };
-
-  return (
-    <div>
-      <h1>Account: CDAC</h1>
-      <h1>Balance: {account.balance}</h1>
-      <input type="button" value="Deposit" onClick={depositHandler} />
-    </div>
-  );
-}
-
-function WithdrawlAccount() {
-  // State is global here
-  let { account } = useSelector((state) => state);
-  let dispatch = useDispatch();
-
-  let withdrawHandler = () => {
-    dispatch(withdraw());
-  };
-
-  return (
-    <div>
-      <h1>Account: CDAC</h1>
-      <h1>Balance: {account.balance}</h1>
-      <input type="button" value="Withdraw" onClick={withdrawHandler} />
+      {list.map((item, index) => (
+        <div key={index} className="alert alert-primary">
+          {item.message}
+        </div>
+      ))}
     </div>
   );
 }
